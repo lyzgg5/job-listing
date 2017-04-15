a27f4873d61e55b4cb1c3453d70a578a2195a445
lyzgg5 <lyzgg5@gmail.com>
2017-04-15 17:17:23 +0800

    step ok

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -43,12 +43,7 @@ class Admin::JobsController < ApplicationController
 
   private
 
-  def require_is_admin
-      if !current_user.admin?
-       flash[:alert] = 'You are not admin'
-       redirect_to root_path
-     end
-   end
+
 
 
   def job_params
