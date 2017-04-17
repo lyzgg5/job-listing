b5946208a62c08dd71e84c006ab4735135f5b783
lyzgg5 <lyzgg5@gmail.com>
2017-04-18 02:09:42 +0800

    admin can see resume list，要注意rootes裡resources寫在那層裡，不然會造成helper路徑不對

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
-    end
+      resources :resumes
+    end    
   end
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
