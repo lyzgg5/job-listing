059977f56a64d25c4ee69afd38e6d2e73dedc7c5
98max <lyzgg5@gmail.com>
2017-04-24 13:59:25 +0800

    套上nic老師的首頁樣式

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
         post :hide
       end
       resources :resumes
-    end    
+    end
   end
-  root 'jobs#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
